1a9a034ec40dee7511610edf5a5f4a239e56490f
JasonLy <jason.ly@gmail.com>
2017-04-15 09:57:23 -0700

    [TASK] completed str reverse

--- jerboa/03-reverse-string-in-place.js
+++ jerboa/03-reverse-string-in-place.js
@@ -1,2 +1,19 @@
 // write a function to reverse a string in place, but since strings are
 // immutable then make a copy first
+
+let reverseStrInPlace = (str) => {
+  let strRev = str.split('');
+  let half = Math.floor(strRev.length/2);
+  let fromBehind = strRev.length - 1;
+
+  for(let i = 0; i < half; i++) {
+    // using es6's swap
+    [strRev[i], strRev[fromBehind - i]] = [strRev[fromBehind - i], strRev[i]];
+  }
+
+  return strRev.join('');
+};
+
+var test = 'what'
+
+reverseStrInPlace(test)
