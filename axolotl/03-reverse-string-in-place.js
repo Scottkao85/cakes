ee8d4653331b24c9311883a35d99ba5c26cc5869
Audrey Magwili <audrey.magwili@level-studios.com>
2017-04-27 11:13:43 -0700

    Updates to 3 and 4

--- axolotl/03-reverse-string-in-place.js
+++ axolotl/03-reverse-string-in-place.js
@@ -3,3 +3,20 @@ Write a function to reverse a string in-place ↴ .
 
 Since strings in JavaScript are immutable ↴ , first convert the string into an array of characters, do the in-place reversal on that array, and re-join that array into a string before returning it. This isn't technically "in-place" and the array of characters will cost O(n)O(n) additional space, but it's a reasonable way to stay within the spirit of the challenge. If you're comfortable coding in a language with mutable strings, that'd be even better!
 */
+
+const reverse = (str) => {
+    let arr = str.split(''),
+    n = arr.length,
+    middle = Math.floor(arr.length / 2),
+    temp = null;
+
+    for (let i = 0; i < middle; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    return arr.join('');
+}
+
+console.log(reverse('abcd'));
