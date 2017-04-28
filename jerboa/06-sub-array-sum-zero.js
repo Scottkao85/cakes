d3aff26ae665ba583c21074e9382833e731ec064
Jason Ly <jly6@apple.com>
2017-04-27 20:00:18 -0700

    [TASK] Other answer but not right

--- jerboa/06-sub-array-sum-zero.js
+++ jerboa/06-sub-array-sum-zero.js
@@ -50,3 +50,29 @@ let sumArrayZero = (arr) => {
 };
 
 sumArrayZero(test);
+
+
+// WTF SUB ARRAYS
+
+// Function to print all sub-arrays with 0 sum present
+// in the given array
+let printallSubarrays = (arr) => {
+  // n is length of the array
+  let n = arr.length;
+
+  // consider all sub-arrays starting from i
+  for (let i = 0; i < n; i++) {
+    let sum = 0;
+    // consider all sub-arrays ending at j
+    for (let j = i; j < n; j++) {
+    // sum of elements so far
+    sum = sum + arr[j];
+      // if sum is seen before, we have found a sub-array with 0 sum
+      if (sum === 0) {
+        console.log("Subarray [" + i + ".." + j + "]");
+      }
+    }
+  }
+}
+
+printallSubarrays([1,2,3,0,-3,-6])
