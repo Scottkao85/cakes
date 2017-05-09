14549a55e20090dc3f4c4626c991293faef570c5
scott kao <scott_kao@apple.com>
2017-05-09 11:40:12 -0700

    added egg drop problem

---
+++ squirrel/04-two-egg.js
@@ -0,0 +1,12 @@
+/*
+A building has 100 floors. One of the floors is the highest floor an egg can be dropped from without breaking.
+If an egg is dropped from above that floor, it will break. If it is dropped from that floor or below, it will be completely undamaged and you can drop the egg again.
+Given two eggs, find the highest floor an egg can be dropped from without breaking, with as few drops as possible.
+*/
+var eggDrop = (egg, floor, breakingPoint) {
+	var result;
+	// var setFloor = Math.floor(Math.random() *100)
+
+
+
+}
